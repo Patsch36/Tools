e82718c85a07a22c01d492ca8318a0245cc6872e
Patrick Scheich <scheichpatrick@gmail.com>
2024-12-17 11:47:25 +0100

    improved file moving/copying

--- cp+.py
+++ cp+.py
@@ -3,37 +3,38 @@ import shutil
 import argparse
 
 
-def copy_file(src, dest):
+def copy_file_or_directory(src, dest):
     if not os.path.exists(src):
-        print(f"Source file '{src}' does not exist.")
+        print(f"Source '{src}' does not exist.")
         return
 
     # Create destination directory if it does not exist
     if not os.path.exists(dest):
-        if len(os.path.splitext(dest.split('/')[-1])[1])>0:
-            if len(dest.split('/'))>1:
-                os.makedirs(os.path.dirname(dest.split('/')[0:-1]))
-        else:
-            os.makedirs(dest)
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
 
-    if os.path.isdir(dest):
+    if os.path.isdir(src):
+        # Copy the directory
         dest = os.path.join(dest, os.path.basename(src))
-
-    # Copy the file
-    shutil.copy2(src, dest)
-    print(f"Copied '{src}' to '{dest}'")
+        shutil.copytree(src, dest)
+        print(f"Copied directory '{src}' to '{dest}'")
+    else:
+        if os.path.isdir(dest):
+            dest = os.path.join(dest, os.path.basename(src))
+        # Copy the file
+        shutil.copy2(src, dest)
+        print(f"Copied file '{src}' to '{dest}'")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Copy a file to a new location.")
-    parser.add_argument("source", help="The source file to copy.")
+        description="Copy a file or directory to a new location.")
+    parser.add_argument("source", help="The source file or directory to copy.")
     parser.add_argument(
-        "destination", help="The destination where the file should be copied.")
+        "destination", help="The destination where the file or directory should be copied.")
 
     args = parser.parse_args()
 
     src = os.path.expanduser(args.source)
     dst = os.path.expanduser(args.destination)
 
-    copy_file(src, dst)
+    copy_file_or_directory(src, dst)
--- mv+.py
+++ mv+.py
@@ -3,37 +3,38 @@ import shutil
 import argparse
 
 
-def move_file(src, dest):
+def move_file_or_directory(src, dest):
     if not os.path.exists(src):
-        print(f"Source file '{src}' does not exist.")
+        print(f"Source '{src}' does not exist.")
         return
 
     # Create destination directory if it does not exist
     if not os.path.exists(dest):
-        if len(os.path.splitext(dest.split('/')[-1])[1])>0:
-            if len(dest.split('/'))>1:
-                os.makedirs(os.path.dirname(dest.split('/')[0:-1]))
-        else:
-            os.makedirs(dest)
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
 
-    if os.path.isdir(dest):
+    if os.path.isdir(src):
+        # Move the directory
         dest = os.path.join(dest, os.path.basename(src))
-
-    # Move the file
-    shutil.move(src, dest)
-    print(f"Moved '{src}' to '{dest}'")
+        shutil.move(src, dest)
+        print(f"Moved directory '{src}' to '{dest}'")
+    else:
+        if os.path.isdir(dest):
+            dest = os.path.join(dest, os.path.basename(src))
+        # Move the file
+        shutil.move(src, dest)
+        print(f"Moved file '{src}' to '{dest}'")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Move a file to a new location.")
-    parser.add_argument("source", help="The source file to move.")
+        description="Move a file or directory to a new location.")
+    parser.add_argument("source", help="The source file or directory to move.")
     parser.add_argument(
-        "destination", help="The destination where the file should be moved.")
+        "destination", help="The destination where the file or directory should be moved.")
 
     args = parser.parse_args()
 
     src = os.path.expanduser(args.source)
     dst = os.path.expanduser(args.destination)
 
-    move_file(src, dst)
+    move_file_or_directory(src, dst)
